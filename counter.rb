8d0da9db21370820f70a662d649c829e980856cd
Chris Marshall <chris@chrismar035.com>
2011-01-30 10:14:10 -0500

    Added counter model, index form

--- counter.rb
+++ counter.rb
@@ -1,11 +1,28 @@
+require 'rubygems'
 require 'sinatra'
-require 'activerecord'
+require 'dm-core'
 
 require 'Hasher.rb'
 
-get '/' do
-  "Counter<br>Count to anything"
+class Counter
+  include DataMapper::Resource
+
+  property :id,             Serial
+  property :title,          String
+  property :description,    Text
+  property :created_at,     DateTime
+  property :incremeting,    Boolean
+  property :current_value,  Integer
+
+  def tick
+    if self.incrementing
+      self.current_value = self.current_value + 1
+    else
+      self.current_value = self.current_value - 1
+    end
+  end
 end
 
-get 'create' do
+get '/' do
+  haml :index
 end
