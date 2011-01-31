335d14d366fcc9d199fa8a4377c6434427c162cf
Chris Marshall <chris@chrismar035.com>
2011-01-31 15:26:22 -0500

    working through creation of counter

--- counter.rb
+++ counter.rb
@@ -1,8 +1,11 @@
 require 'rubygems'
 require 'sinatra'
 require 'dm-core'
+require 'dm-migrations'
 
 require 'Hasher.rb'
+
+DataMapper.setup(:default, 'sqlite::memory:')
 
 class Counter
   include DataMapper::Resource
@@ -11,7 +14,7 @@ class Counter
   property :title,          String
   property :description,    Text
   property :created_at,     DateTime
-  property :incremeting,    Boolean
+  property :incrementing,    Boolean
   property :current_value,  Integer
 
   def tick
@@ -20,9 +23,46 @@ class Counter
     else
       self.current_value = self.current_value - 1
     end
+    self.save unless self.id.nil?
+    self.current_value
+  end
+
+  def hash
+    unless self.id.nil?
+      Hasher::encode(self.id)
+    else
+      nil
+    end
   end
 end
 
 get '/' do
+  DataMapper.auto_migrate!
   haml :index
+end
+
+post '/create' do
+  values = Hash.new
+
+  if params[:starting_value].to_i
+    values[:current_value] = params[:starting_value]
+  end
+
+  values[:incrementing] = params[:direction] == 'Up'
+
+  if params[:title] != ""
+    values[:title] = params{:title}
+  end
+
+  if params[:description] != ""
+    values[:description] = params{:description}
+  end
+
+  counter = Counter.create(values)
+  unless counter.nil?
+    redirect "/#{counter.hash}"
+  else
+    @form_vals= params
+    haml :index
+  end
 end
